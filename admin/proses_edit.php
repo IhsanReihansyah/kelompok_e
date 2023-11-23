31de55a525f8cb24eac302d50c9a472a0ed6730a
Rahma Khairani <rahmarani8@gmail.com>
2023-11-23 16:03:14 +0700

    edit nih dah bisa ya yes

--- admin/proses_edit.php
+++ admin/proses_edit.php
@@ -3,7 +3,6 @@ include 'koneksi.php';
 $judul_film = $_POST["judul_film"];
 $tahun_rilis = $_POST["tahun_rilis"];
 $sinopsis = $_POST["sinopsis"];
-$genre = $_POST["genre"];
 $nama_pemain = $_POST["nama_pemain"];
 $kategori = $_POST["kategori"];
 $nama_sutradara = $_POST["sutradara"];
@@ -17,10 +16,16 @@ if($_FILES["fileToUpload"]["size"]!=0){   // Jika browse image di tekan maka $_F
     } else {
         echo "Sorry, there was an error uploading your file.<br>";
     }
-    $result = mysqli_query($conn, "UPDATE `film` set `judul_film` = '$judul_film', `tahun_rilis` = '$tahun_rilis', `sinopsis` = '$sinopsis', `genre` = '$genre', `nama_pemain` = '$nama_pemain', `id_kategori` = '$kategori', `id_kategori` = '$nama_sutradara', `id_sutradara` = '$rating_film', `gambar`= '$target_file' where `id` = '$_GET[id]'");
+    $result = mysqli_query($conn, "UPDATE `film` set `judul_film` = '$judul_film', `tahun_rilis` = '$tahun_rilis', `sinopsis` = '$sinopsis', `genre` = '$genre', `nama_pemain` = '$nama_pemain', `id_kategori` = '$kategori', `id_sutradara` = '$nama_sutradara', `id_rating` = '$rating_film', `gambar`= '$target_file' where `id` = '$_GET[id]'");
 }
 
-$result = mysqli_query($conn, "UPDATE `film` set `judul_film` = '$judul_film', `tahun_rilis` = '$tahun_rilis', `sinopsis` = '$sinopsis', `genre` = '$genre', `nama_pemain` = '$nama_pemain', `id_kategori` = '$kategori', `id_kategori` = '$nama_sutradara', `id_sutradara` = '$rating_film' where `id` = '$_GET[id]'");
+if (isset($_POST['genre']) && is_array($_POST['genre'])) {
+    $selected_genres = implode(',', $_POST['genre']); // Menggabungkan genre yang dipilih menjadi satu string dipisahkan koma
+} else {
+    $selected_genres = ''; // Atur default jika tidak ada genre yang dipilih
+}
+
+$result = mysqli_query($conn, "UPDATE `film` set `judul_film` = '$judul_film', `tahun_rilis` = '$tahun_rilis', `sinopsis` = '$sinopsis', `genre` ='$selected_genres', `nama_pemain` = '$nama_pemain', `id_kategori` = '$kategori', `id_sutradara` = '$nama_sutradara', `id_rating` = '$rating_film' where `id` = '$_GET[id]'");
 
 header("Location:admin.php");
 
