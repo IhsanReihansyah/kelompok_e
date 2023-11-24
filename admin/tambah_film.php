8f475454b4ca7aa955fbb754586aed605f5441d3
Yesicha Audria <yesichaaudria122@gmail.com>
2023-11-24 21:14:30 +0700

    ubah warna font

--- admin/tambah_film.php
+++ admin/tambah_film.php
@@ -29,9 +29,9 @@
     <?php
     include 'koneksi.php';
     ?>
-        <nav class="sb-topnav navbar navbar-expand navbar-dark" style="background-color: #00237A;">>
+        <nav class="sb-topnav navbar navbar-expand navbar-dark" style="background-color: #00237A;">
             <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3">Dream Movie</a>
+            <a class="navbar-brand ps-3" style="color: #ffffff;">Dream Movie</a>
             <!-- Sidebar Toggle-->
             <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars" style="color: #ffffff;">></i></button>
             <!-- Navbar-->
