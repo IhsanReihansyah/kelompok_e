99d132c33a3280ef7fe15d5e74e41e82b531829b
rehansyah <ihsanreihansyah24@gmail.com>
2023-11-27 17:13:32 +0700

    kepo

--- user/template/navbar.php
+++ user/template/navbar.php
@@ -21,11 +21,6 @@
                                         <li><a href="./kategoriJepang.php">Jepang</a></li>
                                         <li><a href="./kategoriThailand.php">Thailand</a></li>
                                         <li><a href="./kategoriAmerika.php">Amerika</a></li>
-                                        <!-- <li><a href="./anime-details.php">Anime Details</a></li>
-                                        <li><a href="./anime-watching.php">Anime Watching</a></li>
-                                        <li><a href="./blog-details.php">Blog Details</a></li>
-                                        <li><a href="./signup.php">Sign Up</a></li>
-                                        <li><a href="./login.php">Login</a></li> -->
                                     </ul>
                                 </li>
                                 <li><a href="#">Genre <span class="arrow_carrot-down"></span></a>
@@ -41,9 +36,7 @@
                                         <li><a href="./genreFantasy.php">Fantasy</a></li>
                                     </ul>
                                 <li>
-                                <!-- <li><a href="./blog.php">Our Blog</a></li> -->
                                 <li><a href="./about.php">About</a></li>
-                                <!-- <li><a href="./blog.php">Our Blog</a></li> -->
                                 <li><a href="./contact.php">Contact</a></li>
                             </ul>
                         </nav>
