9905806e4aaf27ea6a11c048ce1f3d94bda7fa0a
rehansyah <ihsanreihansyah24@gmail.com>
2023-11-27 17:52:09 +0700

    login di user

--- user/template/navbar.php
+++ user/template/navbar.php
@@ -1,56 +1,59 @@
 <header class="header">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-2">
-                    <div class="header__logo">
-                        <a href="./index.php">
-                            <img src="../img/logo.png" alt="">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-lg-8">
-                    <div class="header__nav">
-                        <nav class="header__menu mobile-menu">
-                            <ul>
-                                <li class="active"><a href="./index.php">Homepage</a></li>
-                                <li><a href="./kategori.php">Categories <span class="arrow_carrot-down"></span></a>
-                                    <ul class="dropdown">
-                                        <li><a href="./kategoriIndonesia.php">Indonesia</a></li>
-                                        <li><a href="./kategoriKorea.php">Korea</a></li>
-                                        <li><a href="./kategoriChina.php">China</a></li>
-                                        <li><a href="./kategoriJepang.php">Jepang</a></li>
-                                        <li><a href="./kategoriThailand.php">Thailand</a></li>
-                                        <li><a href="./kategoriAmerika.php">Amerika</a></li>
-                                    </ul>
-                                </li>
-                                <li><a href="#">Genre <span class="arrow_carrot-down"></span></a>
-                                    <ul class="dropdown">
-                                        <li><a href="./genreRomance.php">Romance</a></li>
-                                        <li><a href="./genreHoror.php">Horor</a></li>
-                                        <li><a href="./genreAdvanture.php">Advanture</a></li>
-                                        <li><a href="./genreAction.php">Action</a></li>
-                                        <li><a href="./genreComedi.php">Comedi</a></li>
-                                        <li><a href="./genreDrama.php">Drama</a></li>
-                                        <li><a href="./genreAnimation.php">Animation</a></li>
-                                        <li><a href="./genreCrime.php">Crime</a></li>
-                                        <li><a href="./genreFantasy.php">Fantasy</a></li>
-                                    </ul>
-                                <li>
-                                <li><a href="./about.php">About</a></li>
-                                <li><a href="./contact.php">Contact</a></li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-                <div class="col-lg-2">
-                    <div class="header__logo">
-                                    <form method="GET" action="kategori.php">
-                                    <input type="text" name="cari" value="<?php if(isset($_GET['cari'])){ echo $_GET['cari']; } ?>">
-                                    </form>
-                           
-                    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1">
+                <div class="header__logo">
+                    <a href="./index.php">
+                        <img src="../img/logo.png" alt="">
+                    </a>
                 </div>
             </div>
-            <div id="mobile-menu-wrap"></div>
+            <div class="col-lg-10">
+                <div class="header__nav">
+                    <nav class="header__menu mobile-menu">
+                        <ul>
+                            <li class="active"><a href="./index.php">Homepage</a></li>
+                            <li><a href="./kategori.php">Categories <span class="arrow_carrot-down"></span></a>
+                                <ul class="dropdown">
+                                    <li><a href="./kategoriIndonesia.php">Indonesia</a></li>
+                                    <li><a href="./kategoriKorea.php">Korea</a></li>
+                                    <li><a href="./kategoriChina.php">China</a></li>
+                                    <li><a href="./kategoriJepang.php">Jepang</a></li>
+                                    <li><a href="./kategoriThailand.php">Thailand</a></li>
+                                    <li><a href="./kategoriAmerika.php">Amerika</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Genre <span class="arrow_carrot-down"></span></a>
+                                <ul class="dropdown">
+                                    <li><a href="./genreRomance.php">Romance</a></li>
+                                    <li><a href="./genreHoror.php">Horor</a></li>
+                                    <li><a href="./genreAdvanture.php">Advanture</a></li>
+                                    <li><a href="./genreAction.php">Action</a></li>
+                                    <li><a href="./genreComedi.php">Comedi</a></li>
+                                    <li><a href="./genreDrama.php">Drama</a></li>
+                                    <li><a href="./genreAnimation.php">Animation</a></li>
+                                    <li><a href="./genreCrime.php">Crime</a></li>
+                                    <li><a href="./genreFantasy.php">Fantasy</a></li>
+                                </ul>
+                            <li><a href="./about.php">About</a></li>
+                            <li><a href="./contact.php">Contact</a></li>
+                            <li>
+                                <form method="GET" action="kategori.php">
+                                    <input type="text" name="cari" value="<?php if (isset($_GET['cari'])) {
+                                        echo $_GET['cari'];
+                                    } ?>">
+                                </form>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+            <div class="col-lg-1">
+                <div class="header__logo">
+                    <a href="../admin/login.php"><span class="icon_profile"></span></a>
+                </div>
+            </div>
         </div>
-    </header>
+        <div id="mobile-menu-wrap"></div>
+    </div>
+</header>
