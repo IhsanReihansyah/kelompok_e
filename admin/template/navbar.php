3ab351c9b8a8e3cf9d9def7c5e8c542c7dc0b2a7
Yesicha Audria <yesichaaudria122@gmail.com>
2023-11-29 21:58:45 +0700

    dropdown admin

--- admin/template/navbar.php
+++ admin/template/navbar.php
@@ -1,28 +1,36 @@
 <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu" style="background-color: #070720;;">
-                        <div class="nav">
-                            <a class="nav-link text-white" href="admin.php">
-                                <div class="sb-nav-link-icon text-white"><i class="fas fa-tachometer-alt"></i></div>
-                                Data Film
-                            </a>
-                            <a class="nav-link text-white" href="data_sutradara.php">
-                                <div class="sb-nav-link-icon text-white"><i class="fas fa-tachometer-alt"></i></div>
-                                Data Sutradara
-                            </a>
-                            <a class="nav-link text-white" href="sutradara.php">
-                                <div class="sb-nav-link-icon text-white"><i class="fas fa-columns"></i></div>
-                                Tambah Sutradara
-                            </a>
-                            <a class="nav-link text-white" href="tambah_film.php">
-                                <div class="sb-nav-link-icon text-white"><i class="fas fa-table"></i></div>
-                                Tambah Film
-                            </a>
-                            <a class="nav-link text-white" href="ks.php">
-                                <div class="sb-nav-link-icon text-white"><i class="fas fa-book-open"></i></div>
-                                Kritik & Saran
-                            </a>
-                        </div>
-                    </div>
-                </nav>
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+        <div class="sb-sidenav-menu" style="background-color: #070720;">
+            <div class="nav">
+                <!-- Dropdown Toggle -->
+                <a class="nav-link text-white" href="admin.php" data-bs-toggle="collapse" data-bs-target="#collapseFilm" aria-expanded="false" aria-controls="collapseFilm">
+                    <div class="sb-nav-link-icon text-white"><i class="fas fa-tachometer-alt"></i></div>
+                    Data Film <i class="fas fa-angle-right dropdown"></i>
+                </a>
+                <!-- Submenu Container -->
+                <div class="collapse sub-menu" id="collapseFilm">
+                    <a href="admin.php" class="sub-item">Berdasarkan Kategori</a>
+                    <a href="#" class="sub-item">Berdasarkan Item 2</a>
+                </div>
+                <!-- End Submenu Container -->
+
+                <a class="nav-link text-white" href="data_sutradara.php">
+                    <div class="sb-nav-link-icon text-white"><i class="fas fa-tachometer-alt"></i></div>
+                    Data Sutradara
+                </a>
+                <a class="nav-link text-white" href="sutradara.php">
+                    <div class="sb-nav-link-icon text-white"><i class="fas fa-columns"></i></div>
+                    Tambah Sutradara
+                </a>
+                <a class="nav-link text-white" href="tambah_film.php">
+                    <div class="sb-nav-link-icon text-white"><i class="fas fa-table"></i></div>
+                    Tambah Film
+                </a>
+                <a class="nav-link text-white" href="ks.php">
+                    <div class="sb-nav-link-icon text-white"><i class="fas fa-book-open"></i></div>
+                    Kritik & Saran
+                </a>
             </div>
+        </div>
+    </nav>
+</div>
