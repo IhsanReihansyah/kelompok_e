43119384f68ffc06b1a60aad5abfe88b8061c456
Yesicha Audria <yesichaaudria122@gmail.com>
2023-11-30 20:17:18 +0700

    mengubah include navbar

--- admin/edit.php
+++ admin/edit.php
@@ -47,12 +47,7 @@
 
     }
     ?>
-    <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <a class="navbar-brand ps-3" href="../user/index.php">Dream Movie</a>
-        <!-- Navbar Brand-->
-        <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
-                class="fas fa-bars"></i></button>
-    </nav>
+    <?php include "template/navbar2.php" ?>
     <div id="layoutSidenav">
     <?php include "template/navbar.php" ?>
         <div id="layoutSidenav_content">
--- admin/edit_sutradara.php
+++ admin/edit_sutradara.php
@@ -34,13 +34,7 @@ include 'koneksi.php';
     $negara = $s["negara"];
 }
 ?>
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="../user/index.php">Dream Movie</a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <!-- Navbar-->
-        </nav>
+    <?php include "template/navbar2.php" ?>
         <div id="layoutSidenav">
         <?php include "template/navbar.php" ?>
             <div id="layoutSidenav_content">
