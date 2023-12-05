c0fb9b0d7b027fc441f089dfd88e6f25d5890b6e
rehansyah <ihsanreihansyah24@gmail.com>
2023-12-05 18:42:31 +0700

    new database

--- eduwork_movie.sql
+++ eduwork_movie.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 05, 2023 at 12:20 PM
+-- Generation Time: Dec 05, 2023 at 12:41 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -65,6 +65,8 @@ CREATE TABLE `film` (
 --
 
 INSERT INTO `film` (`id`, `judul_film`, `tahun_rilis`, `sinopsis`, `tag`, `id_kategori`, `id_sutradara`, `genre`, `nama_pemain`, `gambar`, `video`) VALUES
+(32, 'Train to Busan', '2016-07-15', 'Diceritakan tokoh Seok-woo (Gong Yoo) adalah seorang workaholic yang bercerai. Dia tinggal bersama ibunya dan hampir tidak menghabiskan waktu dengan putrinya Su-an (Kim Su-an).', 'Latest Film,Top Views Month,Slide', 2, 3, 'Romance,Horor,Advanture,Action,Comedi,Drama,Animation,Crime,Fantasy', 'Gong Yoo, Kim Su An', 'images/film-train-busan-2016.jpg.webp', 'nsRcEtGmCdU'),
+(33, 'Avengers: Age of Ultron', '2015-04-13', 'Tony Stark menciptakan Program Ultron untuk melindungi dunia, tetapi ketika program penjaga perdamaian tidak bersahabat, The Avengers bertindak untuk mencoba dan mengalahkan musuh yang hampir mustahil bersama-sama. Pahlawan-pahlawan terkuat di bumi harus bersatu kembali untuk melindungi dunia dari kepunahan global', 'Trending Now,Slide', 6, 3, 'Advanture,Action', 'Chris Evans, Chris Hemsworth, Mark Ruffalo, Robert Downey Jr.', 'images/film-avengers-age-of-ultron-2015.jpg.webp', 'lPpfK1K1qNU'),
 (34, 'TOY STORY', '1995-10-30', 'Seorang anak kecil bernama Andy senang berada di kamarnya sambil bermain-main dengan mainannya, terutama bonekanya yang bernama \"Woody\". Tapi, apa yang dilakukan mainan-mainan itu ketika Andy tidak bersama mereka, mainan-mainan itu menjadi hidup. Woody yakin hidupnya (sebagai mainan) baik. Namun, dia harus khawatir dengan kepindahan keluarga Andy, dan yang tidak diketahui Woody adalah tentang pesta ulang tahun Andy. Woody tidak menyadari bahwa ibu Andy memberinya action figure yang dikenal sebagai Buzz Lightyear, yang tidak percaya bahwa dia adalah mainan, dan dengan cepat menjadi mainan favorit Andy yang baru.', 'Trending Now', 6, 3, 'Advanture,Comedi,Animation', 'Don Rickles, Jim Varney, Tim Allen, Tom Hanks', 'images/film-toy-story-1995.jpg.webp', 'v-PjgYDrg70'),
 (35, 'Justice League', '2017-11-15', 'Dipicu oleh kembalinya keyakinannya pada kemanusiaan dan terinspirasi oleh tindakan tanpa pamrih Superman, Bruce Wayne meminta bantuan sekutu barunya, Diana Prince, untuk menghadapi musuh yang lebih besar. Bersama-sama, Batman dan Wonder Woman bekerja cepat untuk menemukan dan merekrut tim manusia meta untuk melawan ancaman yang baru muncul ini. Namun terlepas dari terbentuknya liga pahlawan yang belum pernah terjadi sebelumnya – Batman, Wonder Woman, Aquaman, Cyborg, dan The Flash – mungkin sudah terlambat untuk menyelamatkan planet ini dari serangan bencana dalam skala besar.', 'Latest Film', 6, 3, 'Advanture,Action,Fantasy', 'Amy Adams, Ben Affleck, Ezra Miller, Gal Gadot, Henry Cavill', 'images/film-justice-league-2017.jpg.webp', 'IAGz2bDlwqA'),
 (36, 'The Moon', '2023-08-02', 'Seorang pria ditinggalkan di luar angkasa karena kecelakaan yang tidak menguntungkan sementara pria lain di Bumi berjuang untuk membawanya kembali dengan selamat.', 'Latest Film', 2, 15, 'Advanture,Drama', 'Kim Hee-ae, Kyung-soo Do, Sol Kyung-gu', 'images/film-the-moon-2023-lk21-d21.jpg.webp', 'OLpqIDNL3eE'),
@@ -74,7 +76,14 @@ INSERT INTO `film` (`id`, `judul_film`, `tahun_rilis`, `sinopsis`, `tag`, `id_ka
 (40, 'may who?', '2015-10-01', 'berkisah tentang seorang remaja yang memiliki kekuatan super. Pong (Thiti Mahayotaruk) adalah seorang remaja yang gemar menghabiskan waktunya seorang diri. Di tengah kesibukannya di sekolah, Pong selalu menyempatkan diri untuk menyelesaikan komik buatannya. Pong mengarang kisah kehidupan pahlawan super dan seorang tokoh protagonis yang menjadi kekasihnya. Karakter tersebut merupakan gambaran dari Ming (Narikun Ketprapakorn), teman sekelas Pong yang membuatnya jatuh hati. Suatu hari, Pong tanpa sengaja mengumpulkan buku sketsa komiknya pada seorang guru. Buku ini kemudian jatuh ke tangan May Nhai (Sutatta Udomsilp) yang juga dikenal sebagai siswa penyendiri. Pong merasa kesal saat May memperlihatkan isi buku tersebut di hadapan teman-teman kelas mereka. Saat Pong berusaha mendapatkan sketsanya kembali, ia terkejut melihat respon May yang mengeluarkan aliran listrik dari tubuhnya.', 'Most Repeat', 5, 14, 'Romance,Comedi,Drama', 'Sutatta Udomsilp, Narikun Ketprapakorn, Thanapob Leeratanakajorn, Thiti Mahayotaruk', 'images/may.jpeg', 'aNHYlWz0Huw'),
 (41, 'Suckseed', '2011-04-20', 'Ped adalah kutu buku di SD yang tidak memiliki minat musik. Sejak teman sekelasnya Ern berbagi kecintaannya pada musik dengannya, Ped jadi menyukai Ern. Tapi orang tua Ern membawanya pergi ke Bangkok membuat mereka berdua terpisah.', 'Top Views Day', 5, 14, 'Comedi', 'Jirayu Laongmanee, Pachara Chirathivat, Thawat Pornrattanaprasert, Nattasha Nauljam', 'images/SuckSeed', 'cOa375cE1Cw'),
 (42, '127 Days', '2023-11-23', 'Film ini diadaptasi dari novel yang berjudul sama karya dari Nadzirah Shafa, diketahui kisah ini dituliskan oleh penulis berdasarkan kisahnya dengan sang suami. Kisah ini mengabadikan perjalanan hijrah dan kisah cintanya selama 172 hari bersama sang suami Amer Azzikra, yang kini telah meninggal dunia.', 'Top Views Day', 1, 13, 'Romance,Drama', ' Bryan Domani, Dan Yasmin Napper', 'images/169866489520398_287x421.jpg', 'IPRBKGxCCZQ'),
-(44, 'Merindu Cahaya de Amstel', '2022-01-20', 'Film ini menceritakan sosok gadis Belanda yang kemudian memeluk agama Islam, bernama Khadija Veenhoven dengan nama asli Marien Veenhoven. Sementara itu, sosok Khadija membuat penasaran Nico, seorang fotografer dan jurnalis, karena mengingatkannya pada sosok sang ibu.', 'Top Views Week', 1, 13, 'Romance,Drama', 'Amanda Rawles, Rachel Amanda, Bryan Domani', 'images/Poster_film_Merindu_Cahaya_de_Amstel.jpeg', 'OW-Bfslos1I');
+(43, 'Sijjin', '2023-11-09', 'Teror itu dibuat oleh Irma yang jatuh cinta kepada sepupunya sendiri yaitu Galang. Galang yang telah memiliki istri dan anak sangat sayang kepada mereka, dan menganggap Irma hanya sebagai sepupunya saja. Oleh karena itu, Irma sangat terobsesi ingin menjadi perempuan satu-satu nya di hidup Galang, bahkan ingin dinikahi. Pada suatu hari, Irma datang ke dukun untuk meminta bantuan agar dapat dinikahkan dengan Galang. Lalu si dukun menerima permohonan Irma yaitu dengan mengirim santet kepada istri Galang dalam 5 teror malam. Teror yang dialami oleh istri Galang ini sangat mengerikan, seperti kesurupan, gangguan mistis, percobaan bunuh diri, dan pada akhirnya kematian terjadi di rumah Galang. Namun tak disangka-sangka juga bahwa teror tersebut juga menghantui pada Irma sendiri.', 'Top Views Week', 1, 13, 'Horor', 'Anggika Bölsterli, Niken Anjani, Ibrahim Risyad ', 'images/Poster_Sijjin.jpg', 'WyU4MbsCjYo'),
+(44, 'Merindu Cahaya de Amstel', '2022-01-20', 'Film ini menceritakan sosok gadis Belanda yang kemudian memeluk agama Islam, bernama Khadija Veenhoven dengan nama asli Marien Veenhoven. Sementara itu, sosok Khadija membuat penasaran Nico, seorang fotografer dan jurnalis, karena mengingatkannya pada sosok sang ibu.', 'Top Views Week', 1, 13, 'Romance,Drama', 'Amanda Rawles, Rachel Amanda, Bryan Domani', 'images/Poster_film_Merindu_Cahaya_de_Amstel.jpeg', 'OW-Bfslos1I'),
+(45, 'Septet: The Story of Hong Kong', '2020-10-21', 'terdapat 7 kisah mandiri yang sangat pribadi dari para sutradara film yang lahir di tahun 40-an dan 50-an untuk mencerminkan Hong Kong yang mereka kenal, dari kisah Sammo tentang pelatihan masa kecilnya dalam seni bela diri, juga mendiang Ringo Lam pada penghormatan pedih untuk kampung halamannya, ode diam-diamnya Ann Hui untuk para pendidik, dan komedi gelap Johnnie To tentang bonanza tiga teman, serta kisah Patrick Tam tentang dua kekasih yang dipisahkan karena satu pindah ke Inggris. ', 'Top Views Month', 3, 16, 'Action,Drama', 'Sammo Hung Au, Kin-yee Patrick, Tam Yuen, ', 'images/Septet-_The_Story_of_Hong_Kong_-_Official_poster.jpg', 'z_ndgu3Rt4Y'),
+(46, 'Our Time Will Come', '2017-07-01', 'Dengan latar pada tahun 1940-an, film ini menceritakan kisah seorang perempuan legendaris \"Fang Gu\" (Zhou Xun), yang merupakan salah satu tokoh kunci selama pendudukan Jepang di Hong Kong. Film ini juga menggambarkan perlawanan dan perjuangan untuk kebebasan dan kemerdekaan oleh kaum muda kelompok perlawanan.', 'Top Views Month', 3, 16, 'Action,Drama', 'Zhou Xun Eddie, Peng Wallace Huo', 'images/our', 'VFT9tE3SjsU'),
+(47, 'Jade Goddess of Mercy', '2003-12-25', 'Ini bercerita tentang seorang gadis biasa yang menjadi polisi yang menyamar dalam regu narkoba.', 'Top Views Year', 3, 16, 'Romance,Crime', 'Zhao Wei Nicholas, Tse Liu Yunlong, Chen Jianbin', 'images/Jade_Goddess_of_Mercy.jpeg', 'Ov0za6Xb1LM'),
+(48, 'My Tomorrow Your Yesterday', '2016-12-17', 'kisah cinta biasa antara sepasang anak muda berusia 20 tahun, Takatoshi Minamiya (Sota Fukushi) jatuh cinta pada pandangan pertama kepada Emi Fukuju (Nana Komatsu) saat berada di satu gerbong kereta yang sama.', 'Top Views Year', 4, 18, 'Romance,Fantasy', 'Sota Fukushi, Nana Komatsu, Masahiro Higashide', 'images/Tomorro.jpeg', 'TLAX78nHQH4'),
+(49, 'My Teacher', '2017-10-28', 'Siswa tahun kedua sekolah menengah dan pencinta olahraga memanah Hibiki Shimada tidak pernah merasakan cinta romantis seumur hidupnya. Ini terjadi meskipun berteman dengan Megumi Chigusa dan Kosuke Kawai, dua remaja yang saling mencintai yang tanpa malu-malu memamerkan perasaan mereka terhadap guru matematika dan seni visual sekolah, Masato Sekiya dan Sachiko Nakajima, masing-masing.', NULL, 4, 18, 'Romance,Comedi,Drama', 'Toma Ikuta, Dan Suzu Hirose', 'images/My_Teacher_poster.jpg', 'HpYqkdn2ilU'),
+(50, 'Yell for the Blue Sky', '2016-08-20', 'Tsubasa Ono (diperankan oleh Tao Tsuchiya) berada di kelas pertama SMA Shirato di Sapporo, Jepang. SMA terkenal dengan tim bisbol dan band kuningan. Sementara Tsubasa Ono melihat trofi untuk band kuningan, ia bertemu Daisuke Yamada (diperankan oleh Ryoma Takeuchi) yang berdiri di sampingnya dan memandangi trofi untuk tim bisbol.', NULL, 4, 18, 'Romance,Drama', 'Tao Tsuchiya, Ryoma Takeuchi, Mirai Shida', 'images/yell', 'bbJUfD8CdnI');
 
 -- --------------------------------------------------------
 
@@ -252,7 +261,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `film`
 --
 ALTER TABLE `film`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=58;
 
 --
 -- AUTO_INCREMENT for table `kategori`
@@ -276,13 +285,13 @@ ALTER TABLE `rating`
 -- AUTO_INCREMENT for table `sutradara`
 --
 ALTER TABLE `sutradara`
-  MODIFY `id_sutradara` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `id_sutradara` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- AUTO_INCREMENT for table `ulasan`
 --
 ALTER TABLE `ulasan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=64;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=65;
 
 --
 -- Constraints for dumped tables
@@ -293,7 +302,7 @@ ALTER TABLE `ulasan`
 --
 ALTER TABLE `film`
   ADD CONSTRAINT `kategori` FOREIGN KEY (`id_kategori`) REFERENCES `kategori` (`id_kategori`),
-  ADD CONSTRAINT `sutradara` FOREIGN KEY (`id_sutradara`) REFERENCES `sutradara` (`id_sutradara`);
+  ADD CONSTRAINT `sutradara` FOREIGN KEY (`id_sutradara`) REFERENCES `sutradara` (`id_sutradara`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `ulasan`
